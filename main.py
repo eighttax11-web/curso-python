1f9a8e51bcf5a2be0de0669bc2ae1126a3087de4
Cristian <eighttax11@gmail.com>
2021-10-21 12:02:38 -0500

    05 - manejo de cadenas2

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-#Cadena
-
+# Cadena
 miGrupoFavorito = "Aerosmith"
 comentario = "The Best Rock Band"
 
-print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
+# print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
+print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+
+numero1 = "1"
+numero2 = "2"
+print("Concatenación:", numero1 + numero2)
+print("Suma:", int(numero1) + int(numero2))  # Cambiar el tipo de dato
