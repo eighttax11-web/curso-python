2fd64f540d25fa3def5950cd24da28f2a3844d77
Cristian <eighttax11@gmail.com>
2021-10-21 20:02:10 -0500

    06 - booleanos / if-else

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# Cadena
-miGrupoFavorito = "Aerosmith"
-comentario = "The Best Rock Band"
+# Tipos bool (boolean)
 
-# print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
-print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+miVariable = True
+print(miVariable)
 
-numero1 = "1"
-numero2 = "2"
-print("Concatenación:", numero1 + numero2)
-print("Suma:", int(numero1) + int(numero2))  # Cambiar el tipo de dato
+miVariable = 3 > 4
+print(miVariable)
+
+if miVariable:
+    print("El resultado es verdadero")
+else:
+    print("El resultado es falso")
