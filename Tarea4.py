623f97f1f3da33eea4981141c10099470e913611
Cristian <eighttax11@gmail.com>
2021-10-22 10:08:10 -0500

    Tarea 4

---
+++ Tarea4.py
@@ -0,0 +1,7 @@
+alto = int(input('Proporciona el alto: '))
+ancho = int(input('Proporciona el ancho: '))
+
+area = alto * ancho
+perimetro = (alto + ancho) * 2
+print(f'Area: {area}')
+print(f'Perímetro: {perimetro}')
