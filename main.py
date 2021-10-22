48e7fc0edb5773dcebc993628204b73dce8bf79f
Cristian <eighttax11@gmail.com>
2021-10-22 10:07:01 -0500

    09 - operadores aritméticos

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-# Función input para procesar la entrada del usuario
-numero1 = int(input("Escribe el primer número: "))
-numero2 = int(input("Escribe el segundo número: "))
-resultado = numero1 + numero2
-print("El resultado de la suma es:", resultado)
+operandoA = 3
+operandoB = 2
+
+suma = operandoA + operandoB
+
+# print('El resultado de la suma es:', suma)
+print(f'El resultado de la suma es: {suma}')
+
+resta = operandoA - operandoB
+print(f'El resultado de la resta es: {resta}')
+
+multiplicacion = operandoA * operandoB
+print(f'El resultado de la multiplicación es: {multiplicacion}')
+
+division = operandoA / operandoB
+print(f'El resultado de la división es: {division}')
+
+division = operandoA // operandoB
+print(f'El resultado de la división (int) es: {division}')
+
+modulo = operandoA % operandoB
+print(f'Resultado residuo división (módulo) es: {modulo}')
+
+exponente = operandoA ** operandoB
+print(f'Resultado exponente: {exponente}')
