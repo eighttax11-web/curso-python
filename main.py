2e3233a26673a295457ac70d96b86c27cf01c759
Cristian <eighttax11@gmail.com>
2021-10-21 20:14:15 -0500

    08 - conversión de la entrada de datos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Función input para procesar la entrada del usuario
-resultado = input("Escribe un mensaje: ")
-print("Valor proporcionado:", resultado)
-print("Fin del programa")
+numero1 = int(input("Escribe el primer número: "))
+numero2 = int(input("Escribe el segundo número: "))
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
