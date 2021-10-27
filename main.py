7ed4541ec1aafb7a10bae7134e53befa1c74f84b
Cristian <eighttax11@gmail.com>
2021-10-26 21:38:17 -0500

    Tarea 13

--- main.py
+++
@@ -1,8 +0,0 @@
-condicion = True
-
-if condicion:
-    print('Condición verdadera')
-else:
-    print('Condición falsa')
-
-print('Condición verdadera') if condicion else print('Condición falsa')  # Operador ternario, usar solo si tenemos pocas lineas de codigo
