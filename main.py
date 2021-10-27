1d0b99b92758f1ad413e6b95bc9425f57850aca7
Cristian <eighttax11@gmail.com>
2021-10-27 13:06:56 -0500

    18 - Palabra continue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-for letra in 'Holanda':
-    if letra == 'a':
-        print(f'Letra encontrada: {letra}')
-        break  # Romper el ciclo
-else:
-    print('Fin ciclo for')
+# for i in range(6):
+#     if i % 2 == 0:
+#         print(f'Valor: {i}')
+
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f'Valor: {i}')
